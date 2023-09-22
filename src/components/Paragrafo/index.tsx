38c6c4429b8778574bce92384daab1d2a8066db9
cleiton lahm <cleijllahm@gmail.com>
2023-09-22 12:54:58 -0300

    a

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -3,8 +3,9 @@ import { P } from './styles';
 
 type Props = {
   children: string;
+  tipo?: 'principal' | 'secundario';
 }
 
-const Paragrafo = (props: Props) => <P>{props.children}</P>
+const Paragrafo = ({children, tipo =  'principal'}: Props) => <P tipo = {tipo}>{children}</P>
 
 export default Paragrafo;
--- src/containers/Sidebar/Sobre/index.tsx
+++ src/containers/Sidebar/Sobre/index.tsx
@@ -4,7 +4,7 @@ import Paragrafo from "../../../components/Paragrafo";
 const Sobre = () => (
   <section>
         <Titulo fontSize={16}>Sobre mim</Titulo>
-        <Paragrafo>
+        <Paragrafo tipo="secundario">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, placeat rerum fuga aut harum ipsum, quam voluptates quisquam amet nostrum expedita soluta qui reiciendis. Beatae odit veniam sint perspiciatis. Sapiente!
         </Paragrafo>
   </section>
